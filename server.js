8b0fba060b392212ecc4eab86dff0cdc6ce551c0
jibin <jokercsi.gmail.com>
2020-08-16 18:18:19 +0900

    connect react with mysql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,37 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (reg, res) => {
-    res.send([
-        {
-            'id': 1,
-            'name' : 'Lee Seung-man'
-        },
-        {
-            'id': 2,
-            'name' : 'Yun Po-sun'
-        },
-        {
-            'id': 3,
-            'name' : 'Park Chung-hee'
-        },
-        {
-            'id': 4,
-            'name' : 'Chun Doo-hwan'
-        },
-        {
-            'id': 5,
-            'name' : 'Roh Tae-woo'
-        },
-        {
-            'id': 6,
-            'name' : 'Kim Young-sam'
-        },
-        {
-            'id': 7,
-            'name' : 'Kim Dae-jung'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
